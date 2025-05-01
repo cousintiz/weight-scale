ac685dda1a1b9a3fdad19a9908c0a5f99cc9187e
augusto <81303751+cousintiz@users.noreply.github.com>
2025-05-01 21:39:14 +0100

    first comit
    first comit

---
+++ Core/Src/main.c
@@ -0,0 +1,558 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "liquidcrystal_i2c.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+#define MEAN_SAMPLES 1024
+#define CAL_SAMPLES 64
+#define THRESHOLD 3
+#define ALFA 0.01
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+ADC_HandleTypeDef hadc1;
+
+I2C_HandleTypeDef hi2c1;
+
+UART_HandleTypeDef hlpuart1;
+
+/* USER CODE BEGIN PV */
+char msg[100];
+uint32_t adc_value = 0;
+double aux = 0.0;
+double mean = 0.0;
+double filter = 0.0;
+double adc_avg = 0.0;
+double weight = 0.0;
+double offset = 0.0;
+double tare_weight = 0.0; /*for tara*/
+bool tara = false; /*tara flag*/
+bool check = false;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_ADC1_Init(void);
+static void MX_LPUART1_UART_Init(void);
+static void MX_I2C1_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+bool is_ready(void);
+uint32_t read_adc(void);
+double get_adc_average(void);
+void get_weight(double adc_mean);
+void show_weight(void);
+void reset_param(void);
+void calibrate(void);
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_ADC1_Init();
+  MX_LPUART1_UART_Init();
+  MX_I2C1_Init();
+  /* USER CODE BEGIN 2 */
+  HD44780_Init(2);
+  HAL_ADCEx_Calibration_Start(&hadc1,ADC_SINGLE_ENDED);
+  calibrate();
+  show_weight(); /*mostrar inicialmente(0.0g)sera reusada*/
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+	  if (is_ready())
+	  {
+		adc_avg = get_adc_average();
+	  	get_weight(adc_avg);
+	  	show_weight();
+
+	  	/*debug*/
+		sprintf(msg, "\r\n Weight: %0.2f g", weight);
+		HAL_UART_Transmit(&hlpuart1,(uint8_t*)msg, strlen(msg), 100);
+
+	  	reset_param();
+	  	HAL_Delay(500);
+	  }
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_MSI;
+  RCC_OscInitStruct.MSIState = RCC_MSI_ON;
+  RCC_OscInitStruct.MSICalibrationValue = 0;
+  RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_6;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_MSI;
+  RCC_OscInitStruct.PLL.PLLM = 1;
+  RCC_OscInitStruct.PLL.PLLN = 32;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
+  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief ADC1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_ADC1_Init(void)
+{
+
+  /* USER CODE BEGIN ADC1_Init 0 */
+
+  /* USER CODE END ADC1_Init 0 */
+
+  ADC_ChannelConfTypeDef sConfig = {0};
+
+  /* USER CODE BEGIN ADC1_Init 1 */
+
+  /* USER CODE END ADC1_Init 1 */
+
+  /** Common config
+  */
+  hadc1.Instance = ADC1;
+  hadc1.Init.ClockPrescaler = ADC_CLOCK_ASYNC_DIV1;
+  hadc1.Init.Resolution = ADC_RESOLUTION_10B;
+  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc1.Init.ScanConvMode = ADC_SCAN_DISABLE;
+  hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+  hadc1.Init.LowPowerAutoWait = DISABLE;
+  hadc1.Init.ContinuousConvMode = DISABLE;
+  hadc1.Init.NbrOfConversion = 1;
+  hadc1.Init.DiscontinuousConvMode = DISABLE;
+  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+  hadc1.Init.DMAContinuousRequests = DISABLE;
+  hadc1.Init.Overrun = ADC_OVR_DATA_PRESERVED;
+  hadc1.Init.OversamplingMode = DISABLE;
+  if (HAL_ADC_Init(&hadc1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Regular Channel
+  */
+  sConfig.Channel = ADC_CHANNEL_1;
+  sConfig.Rank = ADC_REGULAR_RANK_1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_12CYCLES_5;
+  sConfig.SingleDiff = ADC_SINGLE_ENDED;
+  sConfig.OffsetNumber = ADC_OFFSET_NONE;
+  sConfig.Offset = 0;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN ADC1_Init 2 */
+
+  /* USER CODE END ADC1_Init 2 */
+
+}
+
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.Timing = 0x10707DBC;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Analogue filter
+  */
+  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c1, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Digital filter
+  */
+  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c1, 0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
+
+}
+
+/**
+  * @brief LPUART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_LPUART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN LPUART1_Init 0 */
+
+  /* USER CODE END LPUART1_Init 0 */
+
+  /* USER CODE BEGIN LPUART1_Init 1 */
+
+  /* USER CODE END LPUART1_Init 1 */
+  hlpuart1.Instance = LPUART1;
+  hlpuart1.Init.BaudRate = 115200;
+  hlpuart1.Init.WordLength = UART_WORDLENGTH_8B;
+  hlpuart1.Init.StopBits = UART_STOPBITS_1;
+  hlpuart1.Init.Parity = UART_PARITY_NONE;
+  hlpuart1.Init.Mode = UART_MODE_TX_RX;
+  hlpuart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  hlpuart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  hlpuart1.Init.ClockPrescaler = UART_PRESCALER_DIV1;
+  hlpuart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  hlpuart1.FifoMode = UART_FIFOMODE_DISABLE;
+  if (HAL_UART_Init(&hlpuart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_UARTEx_SetTxFifoThreshold(&hlpuart1, UART_TXFIFO_THRESHOLD_1_8) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_UARTEx_SetRxFifoThreshold(&hlpuart1, UART_RXFIFO_THRESHOLD_1_8) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_UARTEx_DisableFifoMode(&hlpuart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN LPUART1_Init 2 */
+
+  /* USER CODE END LPUART1_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOG_CLK_ENABLE();
+  HAL_PWREx_EnableVddIO2();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : Button_Pin */
+  GPIO_InitStruct.Pin = Button_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(Button_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : LD2_Pin */
+  GPIO_InitStruct.Pin = LD2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/**
+  * @brief  calibra e encontra um offset de forma dinamica
+  * @retval none
+  */
+void calibrate(void){
+
+	double avg = 0.0;
+
+	for (int i = 0; i < CAL_SAMPLES ; i++)
+	{
+		avg += get_adc_average();
+		HAL_Delay(10); // remover depois
+	}
+	offset = avg / CAL_SAMPLES;
+}
+
+/**
+  * @brief  mostra o peso no LCD DISPlAY
+  * @retval none
+  */
+void show_weight(void)
+{
+	  sprintf(msg, "%0.2f g ", weight);
+	  HD44780_Clear();
+	  HD44780_SetCursor(2,0);
+	  HD44780_PrintStr("Weight Scale");
+	  HD44780_SetCursor(10,1);
+	  HD44780_SetCursor(4,1);
+	  HD44780_PrintStr(msg);
+	  HD44780_SetCursor(10,1);
+}
+/**
+  * @brief  calcula o peso
+  * @retval none
+  */
+void get_weight(double adc_mean)
+{
+	double aux = 0.0;
+
+	aux = adc_mean - offset;
+
+	if( adc_mean < offset)
+	{
+		weight = 0.0;
+	}else{
+		weight = (7.62941404 * aux + 4.22272973) - tare_weight;
+	}
+}
+
+/**
+  * @brief  calcula a media das leituras do adc
+  * @retval media do adc
+  */
+double get_adc_average(void)
+{
+	uint32_t adc = 0;
+	double sum = 0.0;
+	double average = 0.0;
+
+	for(int i = 0; i < MEAN_SAMPLES; i++)
+	{
+		adc = read_adc();
+		filter = adc*ALFA +(1-ALFA)*filter;
+		sum += filter;
+	}
+	average = sum / (double)MEAN_SAMPLES;
+
+	return average;
+}
+
+
+uint32_t read_adc(void)
+{
+	/*just for test, improve with timers reading periodically*/
+	HAL_ADC_Start(&hadc1);
+	HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY);
+	adc_value = HAL_ADC_GetValue(&hadc1);
+	return adc_value;
+}
+
+/**
+  * @brief  check if adc is reading something....
+  * @retval bool: true if there is an object placed, else false
+  */
+bool is_ready(void)
+{
+	double check_weight = 0.0;
+
+	check_weight = get_adc_average();
+
+	if ( (offset + THRESHOLD ) < check_weight )
+	{
+		return true;
+	}
+	return false;
+}
+
+void reset_param(void)
+{
+	adc_avg = 0;
+	mean = 0;
+	weight = 0.0;
+}
+
+/**
+  * @brief  check if adc is reading something....
+  * @retval bool: true if there is an object placed, else false
+  */
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+	if(GPIO_Pin == Button_Pin)
+	{
+		if(!tara)
+		{
+			tare_weight = weight;
+			tara = true;
+			HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, SET);
+
+		}else
+		{
+			tare_weight = 0.0;
+			tara = false;
+			HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, RESET);
+		}
+	}
+}
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
--- README.md
+++ README.md
@@ -1,1 +1,69 @@
 # weight-scale
+
+## Overview
+
+This project implements a digital weight scale using an STM32L4R5ZI microcontroller. It reads data from a load cell via an ADC, processes the readings, and displays the calculated weight on an I2C LCD. The weight is also transmitted over UART for debugging or data logging.
+
+**Note:** This project was originally developed in 2023 and the code was uploaded to this repository in 2025.
+
+## Hardware
+
+*   **Microcontroller:** STM32L4R5ZI (Based on `.ioc` file and common STM32 naming)
+*   **Load Cell:** Connected to an ADC input (ADC1, Channel 1 inferred from `main.c`). An HX711 amplifier module is commonly used with load cells, but its presence isn't explicitly confirmed in the C code provided.
+*   **Display:** Character LCD connected via I2C (I2C1 inferred from `main.c` and `liquidcrystal_i2c.c`).
+*   **Tare Button:** An external button connected to a GPIO pin with an interrupt (`HAL_GPIO_EXTI_Callback` in `main.c`) to trigger the tare function.
+*   **UART:** LPUART1 used for communication (e.g., debugging output).
+
+## Firmware (`Core/Src/main.c`)
+
+*   **Initialization:** Configures System Clock, GPIOs, ADC (10-bit resolution), I2C1, and LPUART1 using STM32 HAL libraries.
+*   **Calibration:**
+    *   Performs initial ADC self-calibration (`HAL_ADCEx_Calibration_Start`).
+    *   Runs a `calibrate` function on startup. This likely determines the zero-offset by averaging `CAL_SAMPLES` (64) ADC readings.
+*   **Measurement Loop:**
+    *   Continuously checks if the ADC is ready using `is_ready()` (details of this function are not fully visible in the snippet but might involve checking ADC flags or a timer).
+    *   Reads the ADC `MEAN_SAMPLES` (1024) times using `read_adc()`. This function includes a simple low-pass filter (`filter = filter * (1 - ALFA) + adc_value * ALFA;` with ALFA=0.01).
+    *   Calculates the average of these readings in `get_adc_average()`.
+    *   Converts the average ADC reading to weight using `get_weight()`. This function likely applies a linear conversion factor (slope) determined offline and the offset calculated during calibration. The exact formula isn't shown but uses the `offset` variable.
+    *   Displays the calculated weight (in grams, formatted to two decimal places) on the LCD using `show_weight()`.
+    *   Sends the weight value over LPUART1 as a string (e.g., "\r\n Weight: 123.45 g").
+*   **Tare Function:**
+    *   A GPIO interrupt (`HAL_GPIO_EXTI_Callback`) likely triggers the tare functionality.
+    *   Sets a `tara` flag and potentially captures the current weight as `tare_weight`. The `show_weight` function probably subtracts `tare_weight` when the `tara` flag is active.
+*   **Libraries:**
+    *   STM32 HAL Library
+    *   Custom I2C LCD library (`liquidcrystal_i2c.h`/`.c`)
+
+## Calibration Script (`calibration_script.py`)
+
+*   This Python script is a **development utility** and is **not** part of the embedded firmware running on the STM32.
+*   It uses `numpy`, `scipy`, and `matplotlib`.
+*   Takes known ADC readings and corresponding real weights.
+*   Performs linear regression (`np.polyfit`) to find the calibration coefficients (slope and intercept) that relate ADC values to weight.
+*   Can be used to visualize the calibration data and interpolation.
+*   **Usage:** The coefficients (slope) derived from this script are likely hardcoded into the `get_weight` function within the C code. The offset is determined dynamically by the `calibrate` function on the microcontroller.
+
+## Building and Running
+
+*   This project appears to be configured for STM32CubeIDE or a similar Eclipse-based environment (`.cproject`, `.project`, `.ioc`).
+*   Build the project using the IDE.
+*   Flash the firmware onto the STM32L4R5ZI target board using a debugger/programmer (like ST-Link).
+*   Connect a serial terminal (e.g., PuTTY, Tera Term) to LPUART1 to view the weight output.
+
+## Usage
+
+1.  **Power On:** Apply power to the STM32 board.
+2.  **Initialization:** The scale will automatically perform an initial calibration to determine the zero point. The LCD should show "0.00 g" (or similar).
+3.  **Weighing:** Place an object on the load cell platform. The LCD will update to show the measured weight in grams. The same value is sent via LPUART1.
+4.  **Tare:** To zero out the current weight (e.g., if using a container), press the Tare button. The display should return to "0.00 g". Any subsequent weight added will be measured relative to the tared weight. Pressing Tare again might reset the tare value, depending on the exact implementation in `HAL_GPIO_EXTI_Callback`.
+
+## Dependencies (Python Script)
+
+Install necessary Python libraries:
+```bash
+pip install numpy scipy matplotlib
+```
+
+## License
+
+Refer to the `LICENSE` file (MIT License).
